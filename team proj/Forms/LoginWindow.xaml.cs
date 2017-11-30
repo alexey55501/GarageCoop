9f299c95046f3fcee969e28451e4407165113856
08739 <vladislavdeba@example.ru>
2017-11-30 19:37:19 +0200

    trying to fix

--- team proj/Forms/LoginWindow.xaml.cs
+++ team proj/Forms/LoginWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,46 +19,9 @@ namespace team_proj.Forms
     /// </summary>
     public partial class LoginWindow : Window
     {
-        SqlConnection _connection;
-
         public LoginWindow()
         {
             InitializeComponent();
-        }
-
-        private void bEnter_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                Task.Factory.StartNew(() => {
-                    _connection = new SqlConnection
-                    { ConnectionString = "Data Source = 10.3.11.100; Initial Catalog = GarageDb; Integrated Security = True;" };
-                    _connection.Open();
-                    SqlCommand comm = new SqlCommand();
-
-                    comm.CommandText = String.Format("Select Count(*) FROM Employee WHERE Login = '{0}' AND Password = '{1}'", tbLogin.Text, tbPassword.Text);
-
-                    comm.Connection = _connection;
-                    int test = (int)comm.ExecuteScalar();
-                    if (test >= 1)
-                    {
-                        MessageBox.Show("Welcome");
-                    }
-                    else
-                    {
-                        MessageBox.Show("Wrong!");
-                    }
-                });
-               
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
-                _connection.Close();
-            }
         }
     }
 }
